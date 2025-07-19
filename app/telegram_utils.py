47a57c8d635d50e7f04511f45c70f898b2a8f443
AbyssBite <abyssbite@hotmail.com>
2025-07-19 07:43:16 -0600

    Update telegram_utils.py

--- app/telegram_utils.py
+++ app/telegram_utils.py
@@ -2,7 +2,7 @@ import os
 import requests
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
